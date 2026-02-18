6bde16f5c5ee346eab6ef05452c6da567aa8021a
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2026-02-18 04:20:25 +0300

    Fixed password reset

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,6 +33,7 @@ const allowedOrigins = [
    'http://localhost:5174',
    'http://127.0.0.1:5173',
    'http://127.0.0.1:5174',
+   'https://admin-omega-wifi.pages.dev',
    /^http:\/\/192\.168\.\d+\.\d+:\d+$/,
 ];
 
